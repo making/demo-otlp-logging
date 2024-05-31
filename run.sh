e0ebbdd3f1d2e090e0d229165396c37c7093479d
Toshiaki Maki <makingx@gmail.com>
2024-05-31 22:43:11 +0900

    Use lognroll instead of openobserve

--- run.sh
+++ run.sh
@@ -2,10 +2,10 @@
 set -e
 DIR=$(dirname $0)
 ./mvnw spring-boot:run -Dspring-boot.run.jvmArguments="-javaagent:${DIR}/opentelemetry-javaagent.jar \
-                                                         -Dotel.experimental.resource.disabled.keys=process.command_args,process.executable.path,process.runtime.name,process.runtime.version \
+                                                         -Dotel.experimental.resource.disabled.keys=process.command_args,process.executable.path,process.runtime.name,process.runtime.version,process.pid,process.runtime.description,os.type,os.description,host.arch,host.name,service.instance.id,service.version \
                                                          -Dotel.exporter.otlp.compression=gzip \
-                                                         -Dotel.exporter.otlp.endpoint=http://localhost:5080/api/default/ \
-                                                         -Dotel.exporter.otlp.headers='Authorization=Basic cm9vdEBleGFtcGxlLmNvbTpzdlRkaTFtQjltNk1vRHd5' \
+                                                         -Dotel.exporter.otlp.endpoint=http://localhost:4318 \
+                                                         -Dotel.exporter.otlp.headers='Authorization=Bearer changeme' \
                                                          -Dotel.instrumentation.logback-appender.experimental-log-attributes=true \
                                                          -Dotel.instrumentation.logback-appender.experimental.capture-key-value-pair-attributes=true \
                                                          -Dotel.logs.exporter=otlp \
